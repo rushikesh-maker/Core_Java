cdf8f447798472974e54f84023bcf661e2f2ca72
Rushikesh <rockingbrathar@gmail.com>
2022-11-20 15:34:08 +0530

    Default Constructor

---
+++ Java_Programs/src/com/capgemini/constructor/Default.java
@@ -0,0 +1,18 @@
+package com.capgemini.constructor;
+
+class Construct{
+	//This is default constructor
+	public Construct() {
+		System.out.println("This is Default Constructor");
+	}
+}
+
+public class Default {
+
+	public static void main(String[] args) {
+		
+		Construct Ob = new Construct();//Construtor is automatically called at the time of Object creation 
+
+	}
+
+}
