fbdf65a402a43436f2f7ad681f7cd2f1b3f47e51
Rushikesh <rockingbrathar@gmail.com>
2022-11-20 16:58:18 +0530

    commited

--- Java_Programs/src/com/capgemini/multi_threading/Multithread.java
+++ Java_Programs/src/com/capgemini/multi_threading/Multithread.java
@@ -3,13 +3,12 @@ package com.capgemini.multi_threading;
 //Java code for thread creation by implementing
 //the Runnable Interface
 class MultithreadingDemo implements Runnable {
+	
  public void run()
  {
      try {
          // Displaying the thread that is running
-         System.out.println(
-             "Thread " + Thread.currentThread().getId()
-             + " is running");
+         System.out.println("Thread " + Thread.currentThread().getId()+ " is running");
      }
      catch (Exception e) {
          // Throwing an exception
