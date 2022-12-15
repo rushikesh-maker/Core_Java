dba0600e9d375cd5706d1dc91477e24673f47ab2
Rushikesh <rockingbrathar@gmail.com>
2022-12-15 10:45:28 +0530

    finally block

---
+++ Java_Programs/src/com/capgemini/exception_handling/Finally.java
@@ -0,0 +1,27 @@
+package com.capgemini.exception_handling;
+
+public class Finally {
+
+	public static void main(String[] args) {
+		try {
+            System.out.println("inside try block");
+            
+            // Not throw any exception
+            System.out.println(34 / 2);
+        }
+        
+        // Not execute in this case
+        catch (ArithmeticException e) {
+            
+            System.out.println("Arithmetic Exception");
+            
+        }
+        // Always execute
+        finally {
+            
+            System.out.println("finally block always execute.");
+            
+        }
+	}
+
+}
